1d8b10143dad451eacde372b1c33933cb4083441
Ben Biederman <benbiederman@gmail.com>
2022-11-07 07:11:02 -0600

    Add Galaxy Fold index hero.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,19 @@
 import styles from "./Home.module.scss";
+import heroImg from "../../images/hero-img.png";
+import deck from "../../images/product/krispy-kleen-deck.png";
 
 const Home = () => {
   return (
     <main>
-      <section className={styles.hero}></section>
+      <section className={styles.hero}>
+        <img src={heroImg} />
+        <div>
+          <h2>
+            It's time to <span>make</span> skate the donuts.
+          </h2>
+          <button>Shop now</button>
+        </div>
+      </section>
     </main>
   );
 };

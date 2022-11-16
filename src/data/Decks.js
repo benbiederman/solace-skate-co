22da892ed9b70463e295ed96ddf994f24e4d424c
Ben Biederman <benbiederman@gmail.com>
2022-11-16 17:55:52 -0600

    Add alt for skateboards.

--- src/data/Decks.js
+++ src/data/Decks.js
@@ -26,6 +26,7 @@ const Decks = [
       {
         color: "Black",
         img: blackGraffitiDeck,
+        alt: "Black skateboard with black graffiti style logo",
         id: uuidv4(),
         inventory: [{ 7.75: 2, 8.0: 4, 8.25: 3, 8.5: 6 }],
         price: 59.95,
@@ -35,6 +36,7 @@ const Decks = [
       {
         color: "Blue",
         img: blueGraffitiDeck,
+        alt: "Blue skateboard with black graffiti style logo",
         id: uuidv4(),
         inventory: [{ 7.75: 1, 8.0: 5, 8.25: 7, 8.5: 5 }],
         price: 59.95,
@@ -44,6 +46,7 @@ const Decks = [
       {
         color: "Gray",
         img: grayGraffitiDeck,
+        alt: "Gray skateboard with black graffiti style logo",
         id: uuidv4(),
         inventory: [{ 7.75: 0, 8.0: 3, 8.25: 8, 8.5: 7 }],
         price: 59.95,
@@ -53,6 +56,7 @@ const Decks = [
       {
         color: "Green",
         img: greenGraffitiDeck,
+        alt: "Green skateboard with black graffiti style logo",
         id: uuidv4(),
         inventory: [{ 7.75: 1, 8.0: 2, 8.25: 8, 8.5: 9 }],
         price: 59.95,
@@ -62,6 +66,7 @@ const Decks = [
       {
         color: "Purple",
         img: purpleGraffitiDeck,
+        alt: "Purple skateboard with black graffiti style logo",
         id: uuidv4(),
         inventory: [{ 7.75: 1, 8.0: 2, 8.25: 8, 8.5: 9 }],
         price: 59.95,
@@ -71,6 +76,7 @@ const Decks = [
       {
         color: "White",
         img: whiteGraffitiDeck,
+        alt: "White skateboard with black graffiti style logo",
         id: uuidv4(),
         inventory: [{ 7.75: 1, 8.0: 2, 8.25: 8, 8.5: 9 }],
         price: 59.95,
@@ -85,6 +91,7 @@ const Decks = [
       {
         color: null,
         img: krispyKleenDeck,
+        alt: "Krispy Kreme inspired skateboard with donut artwork.",
         id: uuidv4(),
         inventory: [{ 7.75: 7, 8.0: 14, 8.25: 23, 8.5: 31 }],
         price: 59.95,
@@ -99,6 +106,7 @@ const Decks = [
       {
         color: "Black",
         img: blackLogosDeck,
+        alt: "Black skateboard with white logos patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 2, 8.0: 5, 8.25: 11, 8.5: 13 }],
         price: 59.95,
@@ -108,6 +116,7 @@ const Decks = [
       {
         color: "Black on Black",
         img: blackOnBlackLogosDeck,
+        alt: "Black skateboard with black logos patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 3, 8.0: 8, 8.25: 14, 8.5: 21 }],
         price: 59.95,
@@ -117,6 +126,7 @@ const Decks = [
       {
         color: "White",
         img: whiteLogosDeck,
+        alt: "White skateboard with black logos patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 6, 8.0: 3, 8.25: 3, 8.5: 7 }],
         price: 59.95,
@@ -126,6 +136,7 @@ const Decks = [
       {
         color: "White on White",
         img: whiteOnWhiteLogosDeck,
+        alt: "White skateboard with white logos patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 2, 8.0: 5, 8.25: 2, 8.5: 4 }],
         price: 59.95,
@@ -140,6 +151,7 @@ const Decks = [
       {
         color: "Gray",
         img: shapesOne,
+        alt: "White skateboard with gray triangles patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 1, 8.0: 2, 8.25: 0, 8.5: 1 }],
         price: 59.95,
@@ -149,6 +161,7 @@ const Decks = [
       {
         color: "Purple",
         img: shapesTwo,
+        alt: "White skateboard with purple triangles patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 0, 8.0: 1, 8.25: 1, 8.5: 0 }],
         price: 59.95,
@@ -158,6 +171,7 @@ const Decks = [
       {
         color: "Blue",
         img: shapesThree,
+        alt: "White skateboard with blue triangles patterned over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 1, 8.0: 0, 8.25: 0, 8.5: 0 }],
         price: 59.95,
@@ -167,6 +181,7 @@ const Decks = [
       {
         color: "Black",
         img: shapesFour,
+        alt: "White skateboard with black patterns over deck",
         id: uuidv4(),
         inventory: [{ 7.75: 0, 8.0: 1, 8.25: 0, 8.5: 0 }],
         price: 59.95,
@@ -181,6 +196,7 @@ const Decks = [
       {
         color: "Black",
         img: blackTextDeck,
+        alt: "Black skateboard with white lettering spelling out Solace",
         id: uuidv4(),
         inventory: [{ 7.75: 7, 8.0: 22, 8.25: 17, 8.5: 14 }],
         price: 49.95,
@@ -190,6 +206,7 @@ const Decks = [
       {
         color: "Black on Black",
         img: blackOnBlackTextDeeck,
+        alt: "Black skateboard with black lettering spelling out Solace",
         id: uuidv4(),
         inventory: [{ 7.75: 4, 8.0: 19, 8.25: 14, 8.5: 22 }],
         price: 49.95,
@@ -199,6 +216,7 @@ const Decks = [
       {
         color: "White",
         img: whiteTextDeck,
+        alt: "White skateboard with black lettering spelling out Solace",
         id: uuidv4(),
         inventory: [{ 7.75: 3, 8.0: 15, 8.25: 22, 8.5: 17 }],
         price: 49.95,
@@ -208,6 +226,7 @@ const Decks = [
       {
         color: "White on White",
         img: whiteOnWhiteTextDeck,
+        alt: "White skateboard with white lettering spelling out Solace",
         id: uuidv4(),
         inventory: [{ 7.75: 4, 8.0: 13, 8.25: 18, 8.5: 22 }],
         price: 49.95,

3e6d7966796bc1e0f3e42dae17649e498027249e
Ben Biederman <benbiederman@gmail.com>
2022-12-01 07:45:40 -0600

    Add Mobile fold UI for item page.

--- src/components/ItemPage/ItemPage.js
+++ src/components/ItemPage/ItemPage.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./ItemPage.module.scss";
 
 const ItemPage = () => {
   const location = useLocation();
   const [activeItem, setActiveItem] = useState(location.state.item);
   const [inventory, setInventory] = useState(activeItem.inventory);
+  const [activeSize, setActiveSize] = useState();
 
   const itemData = location.state.allData;
   const variations = location.state.variations;
@@ -18,37 +19,66 @@ const ItemPage = () => {
   console.log(inventory);
 
   return (
-    <main className={styles.itemPage}>
-      <div className={styles.itemInfo}>
-        <h2>{itemData.name}</h2>
-        {activeItem.color && <p>{activeItem.color}</p>}
-        {activeItem.sale ? (
-          <p>
-            <span className="sale">${activeItem.price}</span>{" "}
-            <span className="salePrice">${activeItem.salePrice}</span>
-          </p>
-        ) : (
-          <p>${activeItem.price}</p>
-        )}
-        <div className={styles.itemOptions}>
-          {variations &&
-            variations.map((option) => {
+    <main>
+      <section className={styles.itemPage}>
+        {/* Main Product Info */}
+        <div className={styles.itemInfo}>
+          <h2>{itemData.name}</h2>
+          {activeItem.color && <p>{activeItem.color}</p>}
+          {activeItem.sale ? (
+            <p>
+              <span className="sale">${activeItem.price}</span>{" "}
+              <span className="salePrice">${activeItem.salePrice}</span>
+            </p>
+          ) : (
+            <p>${activeItem.price}</p>
+          )}
+          {/* Item Options */}
+          <div className={styles.itemOptions}>
+            {variations &&
+              variations.map((option) => {
+                return (
+                  <img
+                    src={option.img}
+                    alt={option.alt}
+                    tabIndex={0}
+                    key={option.id}
+                  />
+                );
+              })}
+          </div>
+          {/* Sizing Options */}
+          <div className={styles.sizing}>
+            {inventory.map((option) => {
               return (
-                <img
-                  src={option.img}
-                  alt={option.alt}
-                  tabIndex={0}
-                  key={option.id}
-                />
+                <div
+                  tabIndex={option.quantity > 0 ? 0 : -1}
+                  className={
+                    option.quantity > 0
+                      ? styles.sizeAvailable
+                      : styles.sizeUnavailable
+                  }
+                >
+                  <p>{option.size}</p>
+                </div>
               );
             })}
+          </div>
+          {activeSize && activeSize < 5 && (
+            <p className={styles.fomo}>Only X left! Don't miss out!</p>
+          )}
+          <button className={`${styles.cartBtn} primaryBtn`}>
+            Add to cart
+          </button>
+          {/* Add to Cart Button */}
         </div>
-      </div>
-      <div className={styles.itemImg}>
-        <figure>
-          <img src={activeItem.img} alt={activeItem.alt} />
-        </figure>
-      </div>
+        {/* Main Product Image */}
+        <div className={styles.itemImg}>
+          <figure>
+            <img src={activeItem.img} alt={activeItem.alt} />
+          </figure>
+        </div>
+      </section>
     </main>
   );
 };
